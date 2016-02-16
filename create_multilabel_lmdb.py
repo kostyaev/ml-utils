f1d1c39a55c7491f6174eb606cc821f8df5de32d
Dmitry Kostyaev <virtuozz.me@gmail.com>
2016-02-16 18:59:42 +0300

    Write images and labels simultaneously

--- create_multilabel_lmdb.py
+++ create_multilabel_lmdb.py
@@ -4,6 +4,7 @@ import lmdb
 import caffe
 import sys
 import math
+import argparse
 
 def resize(img, maxPx, minPx):
     try:
@@ -24,49 +25,120 @@ def resize(img, maxPx, minPx):
     except IOError as e:
         print "I/O error({0}): {1}".format(e.errno, e.strerror)
 
-def createImageLmdb(filename, images):
+def fillLmdb(images_file, labels_file, images, labels, maxPx, minPx):
     means = np.zeros(3)
-    in_db = lmdb.open(filename, map_size=int(1e12), map_async=True, writemap=True)
-    with in_db.begin(write=True) as in_txn:
-        for in_idx, in_ in enumerate(images):
-            # load image:
-            # - as np.uint8 {0, ..., 255}
-            # - in BGR (switch from RGB)
-            # - in Channel x Height x Width order (switch from H x W x C)
-            im = Image.open(in_)
-            im = resize(im, maxPx=256, minPx=227)
+    cnt = 0
+    images_db = lmdb.open(images_file, map_size=int(1e12), map_async=True, writemap=True)
+    labels_db = lmdb.open(labels_file, map_size=int(1e12))
+
+    images_txn = images_db.begin(write=True)
+    labels_txn = labels_db.begin(write=True)
+
+    examples = zip(images, labels)
+    for in_idx, (image, label) in enumerate(examples):
+        try:
+            #save image
+            im = Image.open(image)
+            im = resize(im, maxPx=maxPx, minPx=minPx)
             im = np.array(im) # or load whatever ndarray you need
             mean = im.mean(axis=0).mean(axis=0)
             means += mean
-
             im = im[:,:,::-1]
             im = im.transpose((2,0,1))
             im_dat = caffe.io.array_to_datum(im)
-            in_txn.put('{:0>10d}'.format(in_idx), im_dat.SerializeToString())
-            if in_idx%1000 == 0:
-                string_ = str(in_idx+1) + ' / ' + str(len(images))
-                sys.stdout.write("\r%s" % string_)
-                sys.stdout.flush()
-    in_db.close()
-    print "Total number of images: {0}, Mean values in RGB order: {1}".format(len(images), means / len(images))
+            images_txn.put('{:0>10d}'.format(in_idx), im_dat.SerializeToString())
 
-
-
-def createLabelLmdb(filename, labels):
-    in_db = lmdb.open(filename, map_size=int(1e12))
-    with in_db.begin(write=True) as in_txn:
-        for in_idx, in_ in enumerate(labels):
-            # load label:
-            # - as np.uint8
-            # - in float and reshape
-            label = np.array(in_).astype(float).reshape(len(in_),1,1)
-            # - Turn to caffe object
+            #save label
+            label = np.array(label).astype(float).reshape(1,1,len(label))
             label_dat = caffe.io.array_to_datum(label)
-            # - Write it
-            in_txn.put('{:0>10d}'.format(in_idx), label_dat.SerializeToString())
-            if in_idx%1000 == 0:
-                string_ = str(in_idx+1) + ' / ' + str(len(labels))
-                sys.stdout.write("\r%s" % string_)
-                sys.stdout.flush()
-    in_db.close()
-    print "Total number of labels: {0}".format(len(labels))
+            labels_txn.put('{:0>10d}'.format(in_idx), label_dat.SerializeToString())
+            cnt += 1
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except Exception as e:
+            print e
+            print "Skipped image and label with id {0}".format(in_idx)
+        if in_idx%500 == 0:
+            string_ = str(in_idx+1) + ' / ' + str(len(images))
+            sys.stdout.write("\r%s" % string_)
+            sys.stdout.flush()
+
+    images_txn.commit()
+    labels_txn.commit()
+    images_db.close()
+    labels_db.close()
+
+    print "\nFilling lmdb completed"
+    print "Image mean values for RBG: {0}".format(means / cnt)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Create lmdb for caffe'
+    )
+    parser.add_argument(
+        '--images',
+        type=str,
+        help='Images file',
+        required=True
+    )
+    parser.add_argument(
+        '-labels',
+        type=str,
+        help='Labels npy file',
+        required=True
+    )
+    parser.add_argument(
+        '--imagesOut',
+        type=str,
+        help='Images lmdb',
+        required=True
+    )
+    parser.add_argument(
+        '-labelsOut',
+        type=str,
+        help='Labels lmdb',
+        required=True
+    )
+    parser.add_argument(
+        '-n',
+        type=int,
+        help='Number of test examples',
+        required=True
+    )
+    parser.add_argument(
+        '--maxPx',
+        type=int,
+        help='Max size of larger dimension after resize',
+        required=True
+    )
+    parser.add_argument(
+        '--minPx',
+        type=int,
+        help='Min size of smaller dimension after resize. Has higher priority than --max',
+        required=True
+    )
+
+    args = parser.parse_args()
+
+    images = np.loadtxt(args.images, str, delimiter='\t')
+    labels = np.load(args.lables)
+
+    print "Creating test set"
+    fillLmdb(
+        images_file=args.imagesOut,
+        labels_file=args.lablesOut,
+        images=images[:args.n],
+        labels=labels[:args.n],
+        minPx=args.minPx,
+        maxPx=args.maxPx)
+
+    print "Creating training set"
+    fillLmdb(
+        images_file=args.imagesOut,
+        labels_file=args.lablesOut,
+        images=images[args.n:],
+        labels=labels[args.n:],
+        minPx=args.minPx,
+        maxPx=args.maxPx)
+
+    print "Completed."
